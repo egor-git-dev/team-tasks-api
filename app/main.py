d8ae5aba5cc471321a2c073351d0f56f3c339d7d
Egor Milehin <emilyohin@yandex.ru>
2026-02-24 14:14:14 +0300

    added hashig for password

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from .database import engine, Base, SessionLocal
 from . import models, schemas
 from sqlalchemy.orm import Session
+from passlib.context import CryptContext
 
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def get_db():
@@ -17,6 +20,10 @@ def get_db():
         db.close()
 
 
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+
 @app.get("/")
 def root():
     return {"message": "Team Tasks API работает"}
@@ -24,14 +31,20 @@ def root():
 
 @app.post("/register", response_model=schemas.UserOut)
 def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = models.User(
+    db_user = db.query(models.User).filter(
+        models.User.email == user.email).first()
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail="Email уже зарегестрирован")
+
+    new_user = models.User(
+        name=user.name,
         email=user.email,
-        hashed_password=user.password,
-        name=user.name
+        hashed_password=get_password_hash(user.password)
     )
 
-    db.add(db_user)
+    db.add(new_user)
     db.commit()
-    db.refresh(db_user)
+    db.refresh(new_user)
 
-    return db_user
+    return new_user
