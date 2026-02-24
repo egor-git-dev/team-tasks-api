a69bd87ed07fabd9b263e7dcfc255424dea1dfdc
Egor Milehin <emilyohin@yandex.ru>
2026-02-24 13:26:05 +0300

    add user registration

--- app/main.py
+++ app/main.py
@@ -1,12 +1,36 @@
-from fastapi import FastAPI
-from .database import engine, Base
-from . import models
+from fastapi import FastAPI, Depends
+from .database import engine, Base, SessionLocal
+from . import models, schemas
+from sqlalchemy.orm import Session
+
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/")
 def root():
     return {"message": "Team Tasks API работает"}
+
+
+@app.post("/register", response_model=schemas.UserOut)
+def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = models.User(
+        email=user.email,
+        hashed_password=user.password
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
